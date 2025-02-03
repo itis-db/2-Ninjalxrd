d2e505b44abd8e72f307479035e6d3201e1e4784
Pavel <143937137+Ninjalxrd@users.noreply.github.com>
2025-02-03 12:28:47 +0300

    Update v3-version.sql

--- v3-version.sql
+++ v3-version.sql
@@ -1,3 +1,19 @@
 /**
   @author
  */
+ALTER TABLE Reader ADD COLUMN age INT;
+ALTER TABLE Book ALTER COLUMN publication_year TYPE VARCHAR(4) USING publication_year::VARCHAR;
+
+ALTER TABLE Book ADD CONSTRAINT unique_isbn UNIQUE (isbn);
+
+ALTER TABLE Author ALTER COLUMN birth_date TYPE VARCHAR(10) USING birth_date::VARCHAR;
+
+ALTER TABLE Genre ADD COLUMN description TEXT DEFAULT 'Нет описания';
+
+ALTER TABLE Reader DROP COLUMN age;
+ALTER TABLE Genre DROP COLUMN description;
+
+ALTER TABLE Book DROP CONSTRAINT unique_isbn;
+
+ALTER TABLE Book ALTER COLUMN publication_year TYPE INT USING publication_year::INT;
+ALTER TABLE Author ALTER COLUMN birth_date TYPE DATE USING birth_date::DATE;
