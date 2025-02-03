2e0748fdd6deaa67bf4df297b1bcafce19d89db9
Pavel <143937137+Ninjalxrd@users.noreply.github.com>
2025-02-03 12:29:42 +0300

    Update v4-cju.sql

--- v4-cju.sql
+++ v4-cju.sql
@@ -1,3 +1,41 @@
 /**
   @author
  */
+WITH recent_books AS (
+    SELECT 
+        title, 
+        publication_year 
+    FROM 
+        Book 
+    WHERE 
+        publication_year::INT > 2000
+)
+SELECT 
+    title, 
+    publication_year 
+FROM 
+    recent_books;
+
+SELECT 
+    Book.title AS book_title, 
+    Author.first_name AS author_first_name, 
+    Author.last_name AS author_last_name, 
+    Publisher.name AS publisher_name
+FROM 
+    Book
+INNER JOIN 
+    Author ON Book.author_id = Author.id
+INNER JOIN 
+    Publisher ON Book.publisher_id = Publisher.id;
+
+SELECT 
+    first_name AS name, 
+    'Reader' AS role 
+FROM 
+    Reader
+UNION ALL
+SELECT 
+    first_name AS name, 
+    'Author' AS role 
+FROM 
+    Author;
